8c5852d5602cd70f5afd727b666af6ebab26225e
James Ferguson <jamesferguson497@gmail.com>
2018-02-03 09:27:54 +1100

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Hello, Vue!'
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #42b883;
+}
+</style>
